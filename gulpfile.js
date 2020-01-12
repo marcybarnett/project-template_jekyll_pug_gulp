a33e381ed4bacc23f195520c0fba61914accee0f
marcybarnett <barnettconcepts@gmail.com>
2020-01-12 00:13:28 -0700

    basic setup working - needs cleanup & more testing

--- gulpfile.js
+++ gulpfile.js
@@ -2,25 +2,34 @@
 // Dependencies
 // --------------------------------------------
 const gulp = require('gulp'),
-    autoprefixer = require('gulp-autoprefixer'),
-    sass = require('gulp-sass'),
-    rename = require('gulp-rename'),
-    terser = require('gulp-terser'),
-    images = require('gulp-imagemin'),
-    strip = require('gulp-strip-comments'),
-    browserSync = require('browser-sync').create();
+  autoprefixer = require('gulp-autoprefixer'),
+  sass = require('gulp-sass'),
+  rename = require('gulp-rename'),
+  terser = require('gulp-terser'),
+  images = require('gulp-imagemin'),
+  strip = require('gulp-strip-comments'),
+  // util = require('gulp-util'),
+  gpug = require('gulp-pug'),
+  // child = require('child_process'),
+  spawn = process.platform === 'win32' ? require('win-spawn') : child.spawn,
+  browserSync = require('browser-sync').create();
 
 // paths
 const styleSrc = './scss/**/*.scss',
-    styleDest = './build/css/',
-    htmlSrc = './*.html',
-    htmlDest = './build/',
-    //vendorSrc = './js/vendors/',
-    //vendorDest = './build/assets/js/',
-    scriptSrc = './js/*.js',
-    scriptDest = './build/js/',
-    imgSrc = './img/',
-    imgDest = './build/img/';
+  styleDest = './_site/assets/css/',
+  // htmlSrc = './*.html',
+  htmlDest = './build/',
+  //vendorSrc = './js/vendors/',
+  //vendorDest = './build/assets/js/',
+  scriptSrc = './js/*.js',
+  scriptDest = './_site/assets/js/',
+  imgSrc = './img/',
+  imgDest = './_site/assets/img/',
+  siteRoot = "./_site",
+  pugSrc = './pug/*.pug',
+  // pugDest = './pug/output/';
+  pugDest = './build/';
+
 
 // --------------------------------------------
 // Stand Alone Tasks
@@ -28,65 +37,95 @@ const styleSrc = './scss/**/*.scss',
 
 // compile scss
 function style() {
-    return gulp.src(styleSrc)
-        .pipe(sass({
-            style: 'compressed'
-        }).on('error', sass.logError))
-        .pipe(autoprefixer())
-        .pipe(rename({
-            basename: 'main',
-            suffix: '.min'
-        }))
-        .pipe(gulp.dest(styleDest))
-        .pipe(browserSync.stream());
+  return gulp.src(styleSrc)
+    .pipe(sass({
+      style: 'compressed'
+    }).on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(rename({
+      basename: 'main',
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest(styleDest))
+    .pipe(browserSync.stream());
 }
 exports.style = style;
 
+// Compile Pug
+function pug() {
+  return gulp.src(pugSrc)
+    .pipe(gpug({
+      pretty: true
+    }))
+    .pipe(strip())
+    .pipe(gulp.dest(pugDest));
+}
+exports.pug = pug;
+
+// Build Site
+function jekyllBuild() {
+  return spawn("bundle", ["exec", "jekyll", "build"], {
+    stdio: "inherit"
+  });
+}
+exports.jekyllBuild = jekyllBuild;
+
+// BrowserSync Reload
+function browserSyncReload(done) {
+  browserSync.reload();
+  done();
+}
+
+
+
 // minify JS
 function minifyScript() {
-    return gulp.src(scriptSrc)
-        .pipe(terser())
-        .pipe(gulp.dest(scriptDest));
+  return gulp.src(scriptSrc)
+    .pipe(terser())
+    .pipe(gulp.dest(scriptDest));
 }
 exports.minifyScript = minifyScript;
 
 // optimize images
 function imageMin() {
-    return gulp.src(imgSrc)
-        .pipe(images())
-        .pipe(gulp.dest(imgDest));
+  return gulp.src(imgSrc)
+    .pipe(images())
+    .pipe(gulp.dest(imgDest));
 }
 exports.imageMin = imageMin;
 
-// strip comments from HTML
+// strip comments from Pug compiled HTML
 function stripHtml() {
-    return gulp.src(htmlSrc)
-        .pipe(strip())
-        .pipe(gulp.dest(htmlDest));
+  return gulp.src(pugDest)
+    .pipe(strip())
+    .pipe(gulp.dest(htmlDest));
 }
 exports.stripHtml = stripHtml;
 
 function watch() {
-    // Serve files from the root of this project or local by flywheel
-    browserSync.init({
-        // proxy: "mbdev.local/",
-        // open: true,
-        server: {
-            baseDir: "./build"
-        }
-    });
+  // Serve files from the root of this project or local by flywheel
+  browserSync.init({
+    // proxy: "mbdev.local/",
+    // open: true,
+    server: {
+      baseDir: siteRoot
+    }
+  });
 
-    // run our tasks
-    gulp.watch(styleSrc, style);
-    gulp.watch(scriptSrc, minifyScript);
-    gulp.watch(imgSrc, imageMin);
-    gulp.watch(htmlSrc, stripHtml);
+  // run our taskss
+  gulp.watch(styleSrc, style);
+  gulp.watch(scriptSrc, minifyScript);
+  gulp.watch(imgSrc, imageMin);
+  gulp.watch(pugSrc, pug);
+  // gulp.watch(pugDest, stripHtml);
+  gulp.watch(htmlDest, gulp.series(jekyllBuild, browserSyncReload));
 
-    // refresh browser
-    gulp.watch(styleDest).on('change', browserSync.reload);
-    gulp.watch(scriptDest).on('change', browserSync.reload);
-    gulp.watch(imgDest).on('change', browserSync.reload);
-    gulp.watch(htmlDest).on('change', browserSync.reload);
+  // refresh browser
+  gulp.watch(styleDest).on('change', browserSync.reload);
+  gulp.watch(scriptDest).on('change', browserSync.reload);
+  gulp.watch(imgDest).on('change', browserSync.reload);
+  // gulp.watch(htmlDest).on('change', browserSync.reload);
+  gulp.watch(siteRoot).on('change', browserSync.reload);
 
 }
 exports.watch = watch;
